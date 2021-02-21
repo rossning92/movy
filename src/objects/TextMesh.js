cab4e8c8e3a823ba16fc6ecde170a7dbe02257fb
rossning92 <rossning92@github.com>
2021-02-20 23:43:13 -0800

    Fix: some symbols are displayed as ??.

--- src/objects/TextMesh.js
+++ src/objects/TextMesh.js
@@ -12,13 +12,13 @@ import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
 const fontLoader = new FontLoader();
 
-const ENGLISH_LETTER_PATT = /^[A-Za-z0-9]*$/;
+const CHINESE_REGEX = /[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]/;
 
 const fontMap = {};
 
 function loadFont(fontName = undefined, letter = undefined) {
   if (fontName === undefined && letter !== undefined) {
-    fontName = ENGLISH_LETTER_PATT.test(letter) ? "en" : "zh";
+    fontName = CHINESE_REGEX.test(letter) ? "zh" : "en";
   }
 
   if (fontName in fontMap) {
