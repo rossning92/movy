1df817c22766d68ab7c50e86a7bf99f021ac6dcf
Ross Ning <rossning92@gmail.com>
2022-03-09 20:09:02 -0800

    Update dolly-zoom.js

--- dist/examples/camera/dolly-zoom.js
+++ dist/examples/camera/dolly-zoom.js
@@ -1,8 +1,11 @@
 import * as mo from 'movy';
 
 mo.usePerspectiveCamera();
-mo.cameraMoveTo({ position: [2, 3, 4], lookAt: [0, 0, 0], duration: 0 });
-mo.addSphere();
+mo.cameraMoveTo({ position: [3, 3, 3], lookAt: [0, 0, 0], duration: 0 });
 mo.addGrid({ color: 'gray', rx: 0 });
 
-mo.camera.dollyZoom({ fov: 10 }).dollyZoom({ fov: 130 });
+for (let i = -3; i <= 3; i += 1) {
+  mo.addSphere({ position: [i, 0, i] });
+}
+
+mo.camera.dollyZoom({ fov: 10 }).dollyZoom({ fov: 90 }).dollyZoom({ fov: 10 });
