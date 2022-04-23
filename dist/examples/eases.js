6190512dc7b1c0e1e9b2d2d61bdd08155c74726e
Ross Ning <rossning92@gmail.com>
2022-04-22 17:06:50 -0700

    Fix examples/eases.js

--- dist/examples/eases.js
+++ dist/examples/eases.js
@@ -3,7 +3,7 @@ import * as mo from 'movy';
 mo.addText('ease: "expo.out"', { x: -6, y: -2, fontSize: 0.3 });
 mo.addImage('images/black-moon.png', { x: -3, y: -2, scale: 2 }).moveTo({
   x: 5,
-  rz: -Math.PI * 4,
+  rz: -720,
   duration: 2,
   ease: 'expo.out',
   t: 0,
@@ -12,7 +12,7 @@ mo.addImage('images/black-moon.png', { x: -3, y: -2, scale: 2 }).moveTo({
 mo.addText('ease: "linear"', { x: -6, y: 2, fontSize: 0.3 });
 mo.addImage('images/black-moon.png', { x: -3, y: 2, scale: 2 }).moveTo({
   x: 5,
-  rz: -Math.PI * 4,
+  rz: -720,
   duration: 1,
   ease: 'linear',
   t: 0,
