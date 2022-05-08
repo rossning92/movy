53aa38bcfac83b5deac87305ceae004bb8815dcc
Ross Ning <rossning92@gmail.com>
2022-05-08 12:15:26 -0700

    Support \white macro for addTex()

--- src/utils/tex.ts
+++ src/utils/tex.ts
@@ -1,19 +1,16 @@
 declare var MathJax: any;
 
-import * as THREE from "three";
-import { toThreeColor } from "./color";
-import { parseSVG } from "./svg";
+import * as THREE from 'three';
+import { toThreeColor } from './color';
+import { parseSVG } from './svg';
 
-export async function tex2svg(
-  tex: string,
-  { color = "white" } = {}
-): Promise<SVGElement> {
+export async function tex2svg(tex: string, { color = 'white' } = {}): Promise<SVGElement> {
   return new Promise((resolve, _) => {
     const input = `\\color{${color}}{${tex}}`;
     MathJax.texReset();
     MathJax.tex2svgPromise(input)
       .then(function (node: any) {
-        const svg = node.getElementsByTagName("svg")[0];
+        const svg = node.getElementsByTagName('svg')[0];
         MathJax.startup.document.clear();
         MathJax.startup.document.updateDocument();
         resolve(svg);
@@ -24,15 +21,15 @@ export async function tex2svg(
 }
 
 function replaceColorMacro(tex: string): string {
-  for (const color of ["red", "green", "blue", "yellow", "cyan", "magenta"]) {
-    tex = tex.replace(new RegExp(`\\\\${color}`, "g"), `\\textcolor{${color}}`);
+  for (const color of ['white', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta']) {
+    tex = tex.replace(new RegExp(`\\\\${color}`, 'g'), `\\textcolor{${color}}`);
   }
   return tex;
 }
 
 export async function createTexObject(
   tex: string,
-  { color = "white" } = {}
+  { color = 'white' } = {}
 ): Promise<THREE.Object3D> {
   tex = replaceColorMacro(tex);
 
@@ -43,8 +40,8 @@ export async function createTexObject(
     const nodes = node.children;
 
     for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].nodeName === "path") {
-        if (nodes[i].getAttribute("d") === "") {
+      if (nodes[i].nodeName === 'path') {
+        if (nodes[i].getAttribute('d') === '') {
           nodes[i].remove();
         }
       } else {
@@ -58,15 +55,15 @@ export async function createTexObject(
   const expandSubSVGElements = (node: Element) => {
     const nodes = node.children;
     for (const node of nodes) {
-      if (node.nodeName === "svg" && node.parentNode !== null) {
-        const translate = `translate(${node.getAttribute("x") || "0"}, ${
-          node.getAttribute("y") || "0"
+      if (node.nodeName === 'svg' && node.parentNode !== null) {
+        const translate = `translate(${node.getAttribute('x') || '0'}, ${
+          node.getAttribute('y') || '0'
         })`;
 
         for (const childNode of node.children) {
-          let transform = childNode.getAttribute("transform");
+          let transform = childNode.getAttribute('transform');
           transform = transform ? `${translate} ${transform}` : translate;
-          childNode.setAttribute("transform", transform);
+          childNode.setAttribute('transform', transform);
           node.parentNode.appendChild(childNode);
         }
 
@@ -81,7 +78,7 @@ export async function createTexObject(
   texObject.traverse((x) => {
     if (x.name) {
       // Remove prefix of MathJax "MJX-*-""
-      x.name = x.name.replace(/MJX-\d+-/g, "");
+      x.name = x.name.replace(/MJX-\d+-/g, '');
     }
   });
 
@@ -90,10 +87,10 @@ export async function createTexObject(
 
 export async function createTexSprite(
   tex: string,
-  { color = "white" } = {}
+  { color = 'white' } = {}
 ): Promise<THREE.Object3D> {
   const canvas = await tex2canvas(tex, {
-    color: "#" + toThreeColor(color as string).getHexString(),
+    color: '#' + toThreeColor(color as string).getHexString(),
   });
   const texture = new THREE.CanvasTexture(canvas);
   const material = new THREE.MeshBasicMaterial({
@@ -113,27 +110,26 @@ export async function createTexSprite(
 
 export async function tex2canvas(
   tex: string,
-  { color = "white" } = {}
+  { color = 'white' } = {}
 ): Promise<HTMLCanvasElement> {
   return new Promise((resolve, _) => {
     const input = `\\color{${color}}{${tex}}`;
     MathJax.texReset();
     MathJax.tex2svgPromise(input)
       .then(function (node: any) {
-        var svg = node.getElementsByTagName("svg")[0];
+        var svg = node.getElementsByTagName('svg')[0];
 
         // Convert to canvas object.
-        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
         var image = new Image();
         image.src =
-          "data:image/svg+xml;base64," +
-          window.btoa(unescape(encodeURIComponent(svg.outerHTML)));
+          'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(svg.outerHTML)));
         image.onload = function () {
-          var canvas = document.createElement("canvas");
+          var canvas = document.createElement('canvas');
 
           canvas.width = image.width * 10;
           canvas.height = image.height * 10;
-          var context = canvas.getContext("2d");
+          var context = canvas.getContext('2d');
           context.drawImage(image, 0, 0, canvas.width, canvas.height);
           resolve(canvas);
         };
